3c8f3db207f5995b390dc53a2febb9f0d8653001
sonpham11235 <sonpham11235@gmail.com>
2025-02-26 18:49:46 +0700

    only encoder left to fix

--- data/graph/bernoulli/prepare.py
+++ data/graph/bernoulli/prepare.py
@@ -1,49 +1,79 @@
 import os
 import requests
 import tiktoken
+import random
 import numpy as np
 
-# download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+def read_file(path):
+    try:
+        data = []
+        with open(path, 'r') as f:
+            for line in f:
+                line.strip()
+                if line:
+                    data.append(line)
+            return data
+    except FileNotFoundError:
+        print(f'Invalid file path: {path}')
+        return []
+    except Exception as e:
+        print(f'An error occured while reading file path: {path}')
+        return []
 
 # positive samples
 possitive_path = os.path.join(os.path.dirname(__file__), 'positive.txt')
 # negative samples
 negative_path = os.path.join(os.path.dirname(__file__), 'negative.txt')
 # edges
-edges = os.path.join(os.path.dirname(__file__), 'edge.txt')
+edges_path = os.path.join(os.path.dirname(__file__), 'edges.txt')
 
 print('Reading precomputed graph paths')
-with open(possitive_path, 'r', encoding='utf-8') as f:
-    positive_data = f.read()
+edges = read_file(edges_path)
+positives = read_file(possitive_path)
+negatives = read_file(negative_path)
 
-with open(possitive_path, 'r', encoding='utf-8') as f:
-    positive_data = f.read()
-
-with open(negative_path, 'r', encoding='utf-8') as f:
-    negative_data = f.read()
-
-n_edges = len()
-n_positive = len(positive_data)
-n_negative = len(negative_data)
+n_edges = len(edges)
+n_positive = len(positives)
+n_negative = len(negatives)
+print(f'Number of edges: {n_edges}')
 print(f'Number of positive samples: {n_positive}')
 print(f'Number of negative samples: {n_negative}')
 
-# train_data = data[:int(n*0.9)]
-# val_data = data[int(n*0.9):]
+print('Preparing train data')
+# add all edges and a fraction of simple path in the train set
+train = edges + random.sample(positives, int(n_positive*0.15))
+# number of current train set
+n_train = len(train)
+# add void edges into the train set. the number match with positive samples
+train += random.sample(negatives, n_train)
+# shuffle the train set
+random.shuffle(train)
+n_train = len(train)
+print(f'Number of train samples {n_train}')
+
+print('Preparing validation data')
+validation = random.sample(edges, int(n_edges*0.1)) + random.sample(positives, int(n_positive*0.015))
+# number of validation
+n_validation = len(validation)
+# add negative samples to validation set
+validation += random.sample(negatives, n_validation)
+# shuffle the validation set
+random.shuffle(validation)
+n_validation = len(validation)
+print(f'Number of validation samples {n_validation}')
 
 # # encode with tiktoken gpt2 bpe
-# enc = tiktoken.get_encoding("gpt2")
-# train_ids = enc.encode_ordinary(train_data)
-# val_ids = enc.encode_ordinary(val_data)
-# print(f"train has {len(train_ids):,} tokens")
-# print(f"val has {len(val_ids):,} tokens")
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train)
+val_ids = enc.encode_ordinary(validation)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
 
-# # export to bin files
-# train_ids = np.array(train_ids, dtype=np.uint16)
-# val_ids = np.array(val_ids, dtype=np.uint16)
-# train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-# val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
 # train.bin has 301,966 tokens
 # val.bin has 36,059 tokens
