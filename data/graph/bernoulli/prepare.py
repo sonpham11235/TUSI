852e45fb1803e636d8d7513037f355b2a90f4ca5
sonpham11235 <sonpham11235@gmail.com>
2025-02-27 23:02:03 +0700

    train and validation set for bernoulli graph

--- data/graph/bernoulli/prepare.py
+++ data/graph/bernoulli/prepare.py
@@ -64,16 +64,16 @@ print(f'Number of validation samples {n_validation}')
 
 # # encode with tiktoken gpt2 bpe
 enc = tiktoken.get_encoding("gpt2")
-train_ids = enc.encode_ordinary(train)
-val_ids = enc.encode_ordinary(validation)
+train_ids = enc.encode_ordinary_batch(train)
+val_ids = enc.encode_ordinary_batch(validation)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
 # export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids = np.concatenate([np.array(sample, dtype=np.uint16) for sample in train_ids])
+val_ids = np.concatenate([np.array(sample, dtype=np.uint16) for sample in val_ids])
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
+# train.bin has 18,054 tokens
+# val.bin has 1,804 tokens
